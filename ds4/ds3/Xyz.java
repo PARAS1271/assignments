6424d350a323cb33f0e3a2fbc671b26a84d4bb23
paras <40178451+PARAS1271@users.noreply.github.com>
2018-10-04 09:43:27 +0530

    Add files via upload

---
+++ ds4/ds3/Xyz.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Xyz
+{
+public static void main(String[] args){
+ArrayList al =new ArrayList();
+al.add(10);
+al.add(15);
+
+al.add(20);
+ArrayList al2 =new ArrayList()
+al2.add(6);
+al2.add(9);
+al2.add(11);
+al.addAll(al2);
+al.retainAll(al2);
+al.removeAll(al2);
+Iterator it =al.iterator 
+}
+
+
